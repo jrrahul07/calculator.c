3a41cbca9ff7289a07ad4538047cc434d3d6affe
jrrahul07 <68863679+jrrahul07@users.noreply.github.com>
2020-07-29 16:10:41 +0530

    Add files via upload

---
+++ calculator...c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int a = 4 ,b = 5 , c ;
+    c=a+b;
+    printf("sum=%d\n",c);
+    c=a-b;
+    printf("difference=%d\n",c);
+    c=a/b;
+    printf("divide=%d \n",c);
+    c=a*b;
+    printf("product=%d\n",c);
+    c=a%b;
+    printf("remainder=%d\n",c);
+
+    return 0 ;
+}
